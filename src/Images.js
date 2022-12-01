e8c4bf4a41295989408432a37129adfd870a675a
Vyacheslav <vyacheslav.dzh@gmail.com>
2022-12-01 14:19:00 +0300

    created utils for easily upload images

---
+++ src/Images.js
@@ -0,0 +1,12 @@
+
+export const plants_page = {
+    background: require('../assets/page-plants__background.png'),
+    card_img: require('../assets/page-plants__item-img.png')
+}
+
+
+
+
+export default {
+    plants_page: plants_page
+}
