4e91c7bd4150bade992e5f22c41609a9d940569d
Vyacheslav <vyacheslav.dzh@gmail.com>
2022-12-02 21:54:23 +0300

    add icons to navigate tab

--- src/Images.js
+++ src/Images.js
@@ -15,8 +15,22 @@ export const main_page =  {
     water_lvl: require('../assets/page-main/water-lvl.png')
 }
 
+export const tab_icons = {
+    inactive : {
+        watering: require('../assets/navigation/icon-watering.png'),
+        main: require('../assets/navigation/icon-main.png'),
+        plants: require('../assets/navigation/icon-plants.png')
+    },
+    active: {
+        watering: require('../assets/navigation/icon-watering-active.png'),
+        main: require('../assets/navigation/icon-main-active.png'),
+        plants: require('../assets/navigation/icon-plants-active.png')
+    }
+}
+
 
 export default {
     plants_page: plants_page,
-    main_page: main_page
+    main_page: main_page,
+    tab_icons: tab_icons
 }
