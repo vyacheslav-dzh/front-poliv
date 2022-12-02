ec9074bdc8bf016de5f561869398871ebaacf327
Vyacheslav <vyacheslav.dzh@gmail.com>
2022-12-02 15:14:27 +0300

    preparing to create navigation

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import PlantsPage from './src/pages/PlantsPage';
 import { NavigationContainer } from '@react-navigation/native';
+import MainPage from './src/pages/MainPage';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <PlantsPage/>
-    </NavigationContainer>
+    <MainPage/>
   );
 }
