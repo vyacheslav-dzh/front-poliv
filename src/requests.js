6b07829d92c0c40a5fc96d85fcabbdf0202afb4d
Vyacheslav <vyacheslav.dzh@gmail.com>
2022-12-25 19:36:43 +0300

    update reqests

--- src/requests.js
+++ src/requests.js
@@ -4,101 +4,123 @@ import { Alert } from "react-native"
 const host = 'http://172.20.10.14'
 const port = '5000'
 const base_url = `${host}:${port}/api`
-const headers = ''
+const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+
+const post_request = (data) => {
+    return {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    }
+}
 
 const requests = {
     pot: {
+        toogle_all: async (value) => {
+            return await fetch(base_url + '/pot/toogle/all', post_request({value: value}))
+            .catch(e => Alert.alert('Connection lost', e))
+        },
+        toogle: async (pot_id) => {
+            return await fetch(base_url + '/pot/toogle/' + pot_id, {
+                method: 'GET'
+            })
+            .catch(e => Alert.alert('Connection lost', e))
+        },
         get_all: async () => {
-            return await fetch(base_url + '/pots', {
+            return await fetch(base_url + '/pot/get/all', {
                 method: 'GET'
             })
             .then(response => response.json())
-            //.then(json => {return json})
+            .then(json => { return json })
             .catch(e => Alert.alert('Connection lost', e))
         },
         get: async (pot_id) => {
-            return await fetch(base_url + '/pot/' + pot_id, {
-                method: 'POST',
+            return await fetch(base_url + '/pot/get/' + pot_id, {
+                method: 'GET',
             })
+            .then(response => response.json())
+            .then(json => { return json })
             .catch(e => Alert.alert('Connection lost', e))
         },
+
         create: async (pot) => {
-            return await fetch(base_url + '/pot/create', {
-                method: 'POST',
-                headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    plant_id: pot.plant_id,
-                    sensor_id: pot.sensor_id,
-                    name: pot.name,
-                    soil_type: pot.soil_type,
-                    date_watering: pot.date_watering,
-                    watering_period: pot.watering_period,
-                    check_wet: pot.check_wet,
-                    wet: pot.wet,
-                })
-            }).then(res => res.status)
+            return await fetch(base_url + '/pot/create', post_request({
+                plant_id: pot.plant_id,
+                name: pot.name,
+                description: pot.description
+            }))
+            .then(response => { return response.status })
             .catch(e => console.log('Connection lost', e))
         },
         delete: async (pot_id) => {
             return await fetch(base_url + '/pot/delete/' + pot_id, {
-                method: 'POST',
+                method: 'GET',
             })
+            .then(response => { return response.status })
             .catch(e => Alert.alert('Connection lost', e))
         },
-        update_period: async (pot_id, watering_period) => {
-            return await fetch(base_url + '/pot/update/period', {
-                method: 'POST',
-                body: {
-                    pot_id: pot_id,
-                    watering_period: watering_period
-                }
-            })
+        update_period: async (pot) => {
+            return await fetch(base_url + '/pot/update', post_request({
+                pot_id: pot.id,
+                watering_period: pot.watering_period,
+                name: pot.name,
+                description: pot.description
+            }))
+            .then(response => { return response.status })
             .catch(e => Alert.alert('Connection lost', e))
         },
         info: async (pot_id) => {
-            return await fetch(base_url + '/requests/Pot/' + pot_id, {
-                method: 'POST'
+            return await fetch(base_url + '/system/pot/info/' + pot_id, {
+                method: 'GET'
             })
-            .then(response => response.json)
+            .then(response => { return response.status })
             .catch(e => Alert.alert('Connection lost', e))
         },
         count: async () => {
-            return await fetch(base_url + '/requests/CountOfPot', {
-                method: 'POST'
-            }).then(response => response.json)
-            .catch(e => Alert.alert('Connection lost', e))
-        },
-        on: async (pot_id) => {
-            return await fetch(base_url + '/requests/StartWatering/' + pot_id, {
-                method: 'POST'
-            }).then(res => {return res.status})
-            .catch(e => Alert.alert('Connection lost', e))
-        },
-        off: async (pot_id) => {
-            return await fetch(base_url + '/requests/EndWatering/' + pot_id, {
-                method: 'POST'
-            }).then(res => {return res.status})
+            return await fetch(base_url + '/system/pot/count', {
+                method: 'GET'
+            })
+            .then(response => { return response.status })
             .catch(e => Alert.alert('Connection lost', e))
         }
     },
     plant: {
         get_all: async () => {
-            return await fetch(base_url + '/plants', {
-                method: 'POST'
+            return await fetch(base_url + '/plant/get/all', {
+                method: 'GET'
             })
             .then(response => response.json())
-            .then(json => {return json})
+            .then(json => { return json })
             .catch(e => console.log('Connection lost', e))
         },
         get: async (plant_id) => {
-            return await fetch(base_url + '/plant/' + plant_id, {
-                method: 'POST'
+            return await fetch(base_url + '/plant/get/' + plant_id, {
+                method: 'GET'
             })
-            .then(response => response.json)
+            .then(response => response.json())
+            .then(json => { return json })
             .catch(e => Alert.alert('Connection lost'))
+        },
+        create: async (plant) => {
+            return await fetch(base_url + '/plant/create', post_request({
+                name: plant.name,
+                description: plant.description,
+                watering_period: plant.watering_period,
+                soil_type: plant.soil_type,
+                wet: plant.wet
+            }))
+            .then(response => { return response.status })
+            .catch(e => Alert.alert('Connection lost', e))
+        },
+        delete: async (plant_id) => {
+            return await fetch(base_url + '/plant/delete/' + plant_id, {
+                method: 'GET'
+            })
+            .then(response => { return response.status })
+            .catch(e => Alert.alert('Connection lost', e))
         }
     },
     sensor: {
@@ -118,16 +140,6 @@ const requests = {
         }
     },
     system: {
-        on: async () => {
-            return await fetch(base_url + '/requests/On', {
-                method: 'POST'
-            }).catch(e => Alert.alert('Connection lost', e))
-        },
-        off: async () => {
-            return await fetch(base_url + '/requests/Off', {
-                method: 'POST'
-            }).catch(e => Alert.alert('Connection lost', e))
-        },
         water: async () => {
             return await fetch(base_url + '/requests/CountofWater', {
                 method: 'POST'
